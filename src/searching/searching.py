0033ca848793c9150528c82b87684e8cb1bd0c50
ExtraJP2014 <extrajp2014@gmail.com>
2020-02-06 22:26:09 -0500

    completed 1 stretch goal

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,23 +1,24 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
-
-   return -1   # not found
+    # TO-DO: add missing code
+    for i in range(len(arr)): 
+        if arr[i] == target: 
+            return i
+    return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    if len(arr) == 0:
+        return -1 # array empty
+    arr = sorted(arr)
+    low = 0
+    high = len(arr)-1
 
-  # TO-DO: add missing code
-
-  return -1 # not found
+    # TO-DO: add missing code
+    return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
